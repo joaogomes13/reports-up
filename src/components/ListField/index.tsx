b93f7836b10d02035d7b3eb16c301e9404dbfa21
JuaumMitty <jjoaovictorpg@gmail.com>
2022-11-13 18:26:14 -0300

    Finalizando componente de lista

--- src/components/ListField/index.tsx
+++ src/components/ListField/index.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchField from '../SearchField';
 import style from './ListField.module.scss';
 import { BiTrash, BiAddToQueue } from 'react-icons/bi';
+import { v4 as uuidv4 } from 'uuid';
 
 
 interface Props {
@@ -10,7 +11,34 @@ interface Props {
 
 export default function ListField({ name }: Props) {
   const [teste, setTeste] = useState(false);
-  const [formTest, setFormTest] = useState([{ equipament: '', qtd: 0 }])
+  const [formTest, setFormTest] = useState([{ id: '',equipament: '', qtd: 0 }])
+
+  const removeEquipament = (id: string) => {
+    let _formTest = [...formTest];
+    _formTest = _formTest.filter(formTest => formTest.id !== id)
+    if(_formTest.length >= 1) {
+      setFormTest(_formTest);
+    } 
+  }
+
+  const addEquipament = () => {
+    let _formTest = [...formTest];
+    _formTest.push({
+      equipament: '',
+      qtd: 0,
+      id: uuidv4()
+    })
+    setFormTest(_formTest);
+  }
+
+  const handleEquipamentChange = (id: string, event: React.ChangeEvent<HTMLInputElement>) => {
+    const index = formTest.findIndex(e => e.id === id);
+
+    let _formTest = [...formTest] as any;
+    _formTest[index][event.target.name] = event.target.value
+
+    setFormTest(_formTest);
+  }
 
   return (
     <fieldset>
@@ -21,11 +49,11 @@ export default function ListField({ name }: Props) {
           {
             formTest.map((element, index) => (
               <div className={style.list__input} key={index}>
-                <input id={name + 'No'} type="text" name={name} value={element.equipament || ''} required placeholder='Equipamento' />
-                <input id={name + 'No'} type="number" name={name} value={element.equipament || 0} required placeholder='Quantidade' />
+                <input id={name + 'No'} type="text" name={name} required placeholder='Equipamento' onChange={(e) => handleEquipamentChange(element.id, e)} />
+                <input id={name + 'No'} type="number" name={name}  required placeholder='Quantidade' onChange={(e) => handleEquipamentChange(element.id, e)} />
                 <div>
-                  <button type='button' className={style.list__input_add}><BiAddToQueue size={24} /></button>
-                  <button type='button' className={style.list__input_remove}><BiTrash size={24} /></button>
+                  <button type='button' className={style.list__input_add} onClick={addEquipament}><BiAddToQueue size={24} /></button>
+                  <button type='button' className={style.list__input_remove} onClick={() => removeEquipament(element.id)}><BiTrash size={24} /></button>
                 </div>
               </div>
             ))
